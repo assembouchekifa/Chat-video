302ae6674509d0dc804d86bd6e5882c10abfd1c1
assembouchekifa <147662333+assembouchekifa@users.noreply.github.com>
2024-03-30 04:12:39 +0100

    first commit

---
+++ client/index.js
@@ -0,0 +1,81 @@
+import { io } from "https://cdn.socket.io/4.7.5/socket.io.esm.min.js";
+
+const socket = io("http://localhost:3000");
+const peer = new Peer();
+let users = [];
+let preeIdClient;
+
+function insertIds(users) {
+  let div = document.getElementById("d1");
+  div.innerHTML = "";
+  users.forEach((e) => {
+    div.innerHTML += `user soketId : ${e.soketId} / user peerId ${e.preeId} <br>`;
+  });
+}
+
+function callUser(preeId) {
+  navigator.mediaDevices
+    .getUserMedia({
+      audio: true,
+      video: true,
+    })
+    .then((stream) => {
+      peer.call(preeId, stream);
+      call.on("stream", (stream) => {
+        videoappend(stream);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+peer.on("open", function (id) {
+  console.log("My peer ID is: " + id);
+  preeIdClient = id;
+  socket.emit("sendPeerId", id);
+});
+
+socket.on("userchang", (data) => {
+  users = data;
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].preeId == preeIdClient) {
+      users.splice(i, 1);
+    }
+  }
+  insertIds(users);
+});
+
+peer.on("call", (call) => {
+  navigator.mediaDevices
+    .getUserMedia({
+      audio: true,
+      video: true,
+    })
+    .then((stream) => {
+      call.answer(stream);
+      call.on("stream", (stream) => {
+        videoappend(stream);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+function videoappend(stream) {
+  let video = document.getElementById("v1");
+  video.srcObject = stream;
+}
+
+navigator.mediaDevices
+  .getUserMedia({
+    audio: true,
+    video: true,
+  })
+  .then((stream) => {
+    videoappend(stream);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
---
+++ server/app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const { createServer } = require("http");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const server = createServer(app);
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const users = [];
+
+io.on("connection", (socket) => {
+  users.push({ name: undefined, soketId: socket.id, peerID: undefined });
+  io.emit("userchang", users);
+  socket.on("sendPeerId", (data) => {
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].soketId == socket.id) {
+        users[i].preeId = data;
+        io.emit("userchang", users);
+      }
+    }
+  });
+  socket.on("disconnect", (data) => {
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].soketId == socket.id) {
+        users.splice(i, 1);
+        io.emit("userchang", users);
+      }
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`server started in port ${port}`);
+});
